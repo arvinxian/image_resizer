24acd37f852d09e441c7f821b03d18d809dd988c
arvinxian <1534001170@qq.com>
2022-05-23 15:22:42 +0800

    add: Scale bar to set and using percentage

--- convertergui.py
+++ convertergui.py
@@ -18,61 +18,60 @@ screen_height = root.winfo_screenheight()
 center_x = int(screen_width/2 - window_width / 2)
 center_y = int(screen_height/2 - window_height / 2)
 
+entryPath = ''
+basewidth = 1000
+
 # set the position of the window to the center of the screen
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-# root.attributes('-alpha', 0.9)
-
-
-# root.withdraw()
-# folder_selected = filedialog.askdirectory()
-# print(folder_selected)
-
-# # place a label on the root window
-# message = tk.Label(root, text=folder_selected)
-# message.pack()
-
-
 def helloCallBack():
+       global entryPath, entryfolder
        folder_selected = filedialog.askdirectory()
        print("I'm clicked")
        
        message = tk.Label(root, text=folder_selected)
        message.pack()
-       # make dir
-       current_directory = os.getcwd()
-       # final_directory = os.path.join(current_directory, r'result')
-       # if not os.path.exists(final_directory):
-       #        os.makedirs(final_directory)
-       
-       basewidth = 500
-
-       # iterate over files in
-       # that directory
        print("folder_selected:" + folder_selected)
+       entryPath = folder_selected[:folder_selected.rindex('/')]
        if folder_selected != '':
-              # create result directory
-              final_directory = os.path.join(folder_selected, r'result')
-              if not os.path.exists(final_directory):
-                     os.makedirs(final_directory)
-              for filename in os.listdir(folder_selected):
-                     
-                     f = os.path.join(folder_selected, filename)
-                     # checking if it is a file
-                     if os.path.isfile(f):
-                            # check if file is an image
-                            filetype = imghdr.what(f)
-                            if (filetype is None or filetype == ''):
-                                   print(f + "is not an image file, skip it")
-                                   continue
-                            img = Image.open(f)
-                            wpercent = (basewidth/float(img.size[0]))
-                            hsize = int((float(img.size[1])*float(wpercent)))
-                            img = img.resize((basewidth, hsize), Image.LANCZOS)
-                            img.save(final_directory + '/' + filename)
-                            print(f)
+              traverse_dir_recur(folder_selected)
        else:
               print("no directory selected")
+              
+              
+def traverse_dir_recur(dir):
+       global entryPath, basewidth
+       dirList = os.listdir(dir)
+       # create result directory
+       final_directory = os.path.expanduser('~') + '/Documents/tmp' + dir[len(entryPath):]
+       if not os.path.exists(final_directory):
+              os.makedirs(final_directory)
+       for dirname in dirList:
+              f = os.path.join(dir, dirname)
+              if os.path.isdir(f):
+                     print("current is directory:" + f)
+                     traverse_dir_recur(dir + "/" + dirname +"/")
+              else:
+                     print(f)
+              # checking if it is a file
+              if os.path.isfile(f):
+                     # check if file is an image
+                     filetype = imghdr.what(f)
+                     if (filetype is None or filetype == ''):
+                            print(f + "is not an image file, skip it")
+                            continue
+                     img = Image.open(f)
+                     if v1.get() < 100.0:
+                            # use setted percentage if scale bar was setted under 100
+                            basewidth = int(float(img.size[0]) * v1.get()/100.0)
+                     
+                     wpercent = (basewidth/float(img.size[0]))
+                            
+                     hsize = int((float(img.size[1])*float(wpercent)))
+                     img = img.resize((basewidth, hsize), Image.LANCZOS)
+                     img.save(final_directory + '/' + dirname)
+                     print(f)
+        
 
 l3 = Label(root, text = "result percentage")
 v1 = DoubleVar()
@@ -85,6 +84,8 @@ button = tk.Button(root, text ="choose folder", command = helloCallBack)
 
 
 s1.pack(anchor = CENTER)
+# defalut value
+s1.set(100) 
 # l3.pack()
 l3.pack()
 # l.pack(pady=(10, 0)) 
